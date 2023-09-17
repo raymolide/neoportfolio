ff37ca8595b9be949d1ae2a0f696d19a942545a7
raymolide <raymolide@gmail.com>
2023-09-17 17:32:23 +0200

    uppercase resume btn and nav items

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -6,7 +6,7 @@ function NavBar() {
             <div className="logo hidden lg:block font-extrabold font-sans">
                 MOLIDE R.
             </div>
-            <ul className="flex flex-row  space-x-4 font-sans font-bold text-center gap-1 lg:gap-5 ">
+            <ul className="flex flex-row  space-x-2 font-sans font-bold text-center gap-1 lg:gap-5 ">
                 <li><a href="#home" className="hover:text-white hover:bg-primary p-2 border-b border-black border uppercase">Home</a></li>
                 <li><a href="#about" className="hover:text-white hover:bg-primary p-2 border-b border-black border uppercase">About</a></li>
                 <li><a href="#skills" className="hover:text-white hover:bg-primary p-2 border-b border-black border uppercase">Skills</a></li>
