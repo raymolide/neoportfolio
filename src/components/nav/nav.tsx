fc17eb3e1b3dcd18d271d411ebd21bcef1fc7d01
raymolide <raymolide@gmail.com>
2023-09-17 17:29:12 +0200

    uppercase resume btn and nav items

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -8,7 +8,7 @@ function NavBar() {
             </div>
             <ul className="flex flex-row  space-x-4 font-sans font-bold text-center gap-1 lg:gap-5 ">
                 <li><a href="#home" className="hover:text-white hover:bg-primary p-2 border-b border-black border uppercase">Home</a></li>
-                <li><a href="#about" className="hover:text-white hover:bg-primary p-2 border-b border-black borderuppercase">About</a></li>
+                <li><a href="#about" className="hover:text-white hover:bg-primary p-2 border-b border-black border uppercase">About</a></li>
                 <li><a href="#skills" className="hover:text-white hover:bg-primary p-2 border-b border-black border uppercase">Skills</a></li>
                 <li><a href="#projects" className="hover:text-white hover:bg-primary p-2 border-b border-black border uppercase">Projects</a></li>
             </ul>
